277c45a09e45659c429235a96d7c40a072e17311
Adexuz <adexuz7@gmail.com>
2020-02-06 15:33:10 +0000

    Update CharacterList.java

--- src/main/java/dad/fxrevenge/dialog/character/CharacterList.java
+++ src/main/java/dad/fxrevenge/dialog/character/CharacterList.java
@@ -15,9 +15,9 @@ public class CharacterList {
 	private static Character javaGoddess = new Character("Diosa Java", new Image("/image/dialog/character/java_goddess.png"), false);
 
 	// Villanos
-	private static Character M = new Character("M", new Image("/image/dialog/character/m.png"), false);
-	private static Character V = new Character("V", new Image("/image/dialog/character/v.png"), false);
-	private static Character C = new Character("C", new Image("/image/dialog/character/c.png"), false);
+	private static Character M = new Character("Meridio", new Image("/image/dialog/character/m.png"), false);
+	private static Character V = new Character("Valery", new Image("/image/dialog/character/v.png"), false);
+	private static Character C = new Character("Calipso", new Image("/image/dialog/character/c.png"), false);
 	private static Character FX = new Character("Mr. FX", new Image("/image/dialog/character/fx.png"), false);
 	
 	// Getters
