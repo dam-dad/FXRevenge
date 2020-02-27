c6abdafb439f738b27fd1121bced6e694a6811fe
Aquaelix <jimenezsacramentoadan@gmail.com>
2020-02-27 15:53:12 +0000

    Add code to view player stats

---
+++ src/main/java/dad/fxrevenge/menu/stats/PlayerController.java
@@ -0,0 +1,118 @@
+package dad.fxrevenge.menu.stats;
+
+import java.io.IOException;
+
+import dad.fxrevenge.models.Avatar;
+import dad.fxrevenge.scene.GameScene;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class PlayerController extends BorderPane implements GameScene {
+
+	//model
+	private Avatar pj;
+	
+	@SuppressWarnings("unused")
+	private Scene scene;
+
+    @FXML
+    private BorderPane view;
+
+    @FXML
+    private ImageView playerImage;
+
+    @FXML
+    private VBox datosBox;
+
+    @FXML
+    private GridPane datosGrid;
+
+    @FXML
+    private TextField nivelTextField;
+
+    @FXML
+    private TextField experienciaTextField;
+
+    @FXML
+    private TextField vidaTextField;
+
+    @FXML
+    private TextField manaTextField;
+
+    @FXML
+    private TextField ataqueFisTextField;
+
+    @FXML
+    private TextField ataqueMagTextField;
+
+    @FXML
+    private TextField defFisTextField;
+
+    @FXML
+    private TextField defMagTextField;
+
+    @FXML
+    private TextField criticoTextField;
+
+    @FXML
+    private TextField dineroTextField;
+
+    @FXML
+    private HBox tituloHBox;
+
+    @FXML
+    private Label tituloLabel;
+
+    @FXML
+    private Label notaLabel;
+
+	@Override
+	public void start() {
+
+		scene = new Scene(view);
+
+		// seteos de la informacion
+		playerImage.imageProperty().bind(pj.combatSpriteProperty());
+		
+		tituloLabel.textProperty().bind(pj.nameProperty().concat(" ("+pj.getWork()+")"));
+		nivelTextField.textProperty().bind(pj.levelProperty().asString());
+		experienciaTextField.textProperty().bind(pj.currentExpProperty().asString().concat("/").concat(pj.totalLevelExpProperty().asString()));
+		vidaTextField.textProperty().bind(pj.currentLifeProperty().asString().concat("/").concat(pj.HealthProperty().asString()));
+		manaTextField.textProperty().bind(pj.currentManaProperty().asString().concat("/").concat(pj.ManaProperty().asString()));
+		ataqueFisTextField.textProperty().bind(pj.PhysDamageProperty().asString());
+		ataqueMagTextField.textProperty().bind(pj.MagicDamageProperty().asString());
+		defFisTextField.textProperty().bind(pj.PhysDefProperty().asString());
+		defMagTextField.textProperty().bind(pj.MagicDefProperty().asString());
+		criticoTextField.textProperty().bind(pj.CritChanceProperty().asString());
+		dineroTextField.textProperty().bind(pj.moneyProperty().asString());
+		
+	}
+
+	public PlayerController(Avatar pj) throws IOException {
+		super();
+
+		this.pj=pj;
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/PlayerView.fxml"));
+		loader.setController(this);
+		loader.setRoot(this);
+		loader.load();
+	}
+
+	public BorderPane getView() {
+		return view;
+	}
+
+	@Override
+	public void stop() {
+	}
+
+}
---
+++ src/main/java/dad/fxrevenge/menu/stats/PlayerMain.java
@@ -0,0 +1,17 @@
+package dad.fxrevenge.menu.stats;
+
+/**
+ * Clase de pruebas para el componente BestiaryController
+ * @author Adan
+ *
+ */
+public class PlayerMain {
+	
+	/**
+	 * Llamada a la clase que lanzará el BestiaryController
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		PlayerApp.main(args);
+	}
+}
