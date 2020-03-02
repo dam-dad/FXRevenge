25cd7f8901f73db3972205ea6d9403b475f76b3d
Adexuz <adexuz7@gmail.com>
2020-03-02 13:09:45 +0000

    Added vendor image to overworld map

--- src/main/resources/map/World/mage_f.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="mage_f" tilewidth="32" tileheight="32" tilecount="12" columns="3">
- <image source="../../Image/characters/mage_f.png" width="96" height="144"/>
-</tileset>
